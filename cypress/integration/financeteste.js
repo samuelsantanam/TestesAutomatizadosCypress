7e9519e45ab173be4f565815bb6d27bb09c09bb9
samuelsantanam <s.santanamarinho@gmail.com>
2024-03-14 12:13:24 -0300

    Removendo projetos antigos

--- cypress/integration/financeteste.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-context('Dev Finances', () => {
-    it('Cadastrar transacao', () => {
-        cy.visit('https://dev-finance.netlify.app')
-        cyp.get('#transation .button').click
-    });
-    
-});
