06d7a1f9d59a526cb9d1c82c5afe66e5a39e2d75
samuelsantanam <s.santanamarinho@gmail.com>
2024-03-14 12:16:23 -0300

    Ajuste da função login;

--- cypress/support/Login/login.js
+++ cypress/support/Login/login.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add('login_teste',(user,password)=>{
-    cy.visit('https://www.saucedemo.com/')
+    cy.visit('https://www.google.com/')
     cy.get('[data-test="username"]').type(user);
     cy.get('[data-test="password"]').type(password);
     cy.get('[data-test="login-button"]').click()
